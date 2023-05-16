6885733423d18417132999ce1e735d37e9706a8d
Sinem Ergin <sinemergin71@hotmail.com>
2023-05-16 21:37:14 +0300

    first line added

---
+++ main.py
@@ -0,0 +1,4 @@
+from win10toast import ToastNotifier
+
+bildirim = ToastNotifier()
+bildirim.show_toast(title="Selam ben Sinem!", msg="Beni İnstagram üzerinden takip edebilirsiniz! Kullanıcı adım: @asinemergnn", icon_path="C:\profile-pic.ico", duration=5)
